b271fca79857ae29a2262e28799569b3f3dfe9d3
henriquelavor <henriquelavor@gmail.com>
2019-06-07 12:26:49 -0400

    Inicio commit -  Projeto criado

---
+++ src/main/java/com/henriquelavor/agendamongodb/AgendamongodbApplication.java
@@ -0,0 +1,13 @@
+package com.henriquelavor.agendamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamongodbApplication.class, args);
+	}
+
+}
