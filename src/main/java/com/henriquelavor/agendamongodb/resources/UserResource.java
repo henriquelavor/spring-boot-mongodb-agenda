f9915d0bf4680f00bc849f7de0a008705a4c7a1b
henriquelavor <henriquelavor@gmail.com>
2019-06-08 09:59:23 -0400

    Usando o padrao DTO para retorna Usuarios

--- src/main/java/com/henriquelavor/agendamongodb/resources/UserResource.java
+++ src/main/java/com/henriquelavor/agendamongodb/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.henriquelavor.agendamongodb.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.henriquelavor.agendamongodb.domain.User;
+import com.henriquelavor.agendamongodb.dto.UserDTO;
 import com.henriquelavor.agendamongodb.services.UserService;
 
 @RestController
@@ -22,8 +24,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
